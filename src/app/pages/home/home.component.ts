06f34a91ebb99dc76b75941a9bb1d310ede0862b
amandalimon <amandalimon@outlook.com>
2024-06-17 14:46:58 -0600

    update tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -44,6 +44,20 @@ export class HomeComponent {
   }
 
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+    this.tasks.update((task) => task.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
   }
 }
